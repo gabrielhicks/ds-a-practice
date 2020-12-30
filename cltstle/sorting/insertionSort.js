48c53cbf8deb58cdd19081b499c5c80387c7ec85
gabrielhicks <gabrielhicks@mail.com>
2020-12-30 15:33:04 -0500

    insertionSort

---
+++ cltstle/sorting/insertionSort.js
@@ -0,0 +1,10 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > current; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
