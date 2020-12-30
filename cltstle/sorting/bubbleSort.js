7ac7c97252287fb04e40e7312db08318c9785d76
gabrielhicks <gabrielhicks@mail.com>
2020-12-30 15:15:48 -0500

    optimize bubble, add selectionSort

--- cltstle/sorting/bubbleSort.js
+++ cltstle/sorting/bubbleSort.js
@@ -2,15 +2,19 @@ function bubbleSort(array) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
+  let skipSwap;
   for (let i = array.length; i > 0; i--) {
+    skipSwap = true;
     for (let j = 0; j < i - 1; j++) {
       if (array[j] > array[j + 1]) {
         // let temp = array[j];
         // array[j] = array[j + 1];
         // array[j + 1] = temp;
+        skipSwap = false;
         swap(array, j, j + 1);
       }
     }
+    if (skipSwap) break;
   }
   return array;
 }
---
+++ cltstle/sorting/selectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(array) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) min = j;
+    }
+    if (i !== min) swap(array, i, min);
+  }
+  return array;
+}
